bd84c9c0002940832015438820bb8fabb821cf57
shad0w-cat <harshy.167@gmail.com>
2022-01-18 23:15:40 +0530

    not confirmed changes

--- assets/js/yt.js
+++ assets/js/yt.js
@@ -10,8 +10,8 @@ var player;
 
 function onYouTubeIframeAPIReady() {
     player = new YT.Player('playerab', {
-        height: '390',
-        width: '716.8',
+        height: '400rem',
+        width: '100%',
         videoId: 'w6qtmKg8TTw',
         playerVars: {
             'playsinline': 1,
@@ -24,7 +24,8 @@ function onYouTubeIframeAPIReady() {
             'disablekb': 1,
             'widget_referrer': 1,
             'controls': 0,
-            'fs': 0
+            'fs': 0,
+
         },
         events: {
             'onReady': onPlayerReady,
@@ -35,6 +36,7 @@ function onYouTubeIframeAPIReady() {
 
 function onPlayerReady(event) {
     event.target.playVideo();
+
 }
 var done = false;
 
@@ -42,8 +44,8 @@ function onPlayerStateChange(event) {
     if (event.data == YT.PlayerState.PLAYING && !done) {
         setTimeout(stopVideo, 76000);
         done = true;
+        remove();
     }
-    console.log("hello")
     const videoIcon = document.getElementById('video-icon');
     let playerState = player.getPlayerState();
     if (playerState == 1) {
@@ -79,3 +81,32 @@ function playPause() {
         videoIcon.classList.remove('ri-play-line')
     }
 }
+
+/*function remove() {
+    // $("#playerab").contents().find(".ytp-button").remove();
+    // $("#playerab").contents().find(".ytp-large-play-button").remove();
+
+    // $("#playerab").contents().find("").remove();
+    let iframevar = document.getElementById('playerab');
+    let elements = iframevar.contentWindow.document.getElementsByClassName("ytp-show-cards-title");
+    console.log(elements)
+    while (elements.length > 0) {
+        elements[0].style.display = 'none';
+    }
+
+    // let elements = document.getElementsByClassName('ytp-large-play-button');
+    // while (elements.length > 0) {
+    //     elements[0].parentNode.removeChild(elements[0]);
+    // }
+    // elements = document.getElementsByClassName('ytp-chrome-top');
+    // while (elements.length > 0) {
+    //     elements[0].parentNode.removeChild(elements[0]);
+    // }
+    // elements = document.getElementsByClassName('ytp-watermark');
+    // while (elements.length > 0) {
+    //     elements[0].parentNode.removeChild(elements[0]);
+    // }
+
+    // document.getElementsByClassName("ytp-watch-later-button ytp-button ytp-show-watch-later-title")[0].style.display = 'none'
+    // document.getElementsByClassName("ytp-button ytp-share-button ytp-share-button-visible ytp-show-share-title")[0].style.display = 'none'
+}*/
